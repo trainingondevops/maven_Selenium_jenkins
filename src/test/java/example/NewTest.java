cd2e0a4df1de7012d366e2628a611ce668c546f8
trainingondevops <training.devops17@gmail.com>
2018-01-04 13:20:21 -0500

    Maven with selenium
    Maven & Jenkins with Selenium: Complete Tutorial

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,30 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NewTest {
+	private WebDriver driver;
+
+	@Test				
+	public void testEasy() {	
+		driver.get("http://www.guru99.com/selenium-tutorial.html");
+		String title = driver.getTitle();				 
+		AssertJUnit.assertTrue(title.contains("Free Selenium Tutorials")); 		
+	}	
+	
+	@BeforeTest
+	public void beforeTest() {	
+	    driver = new FirefoxDriver();  
+	}		
+	
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}
+
+}
